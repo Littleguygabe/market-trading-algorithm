9b0b109fd4cca4c8926c9cd4c36e0ea8930a92ad
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-09-03 10:25:20 +0100

    Remove unused DMatrix creation in xgboost_trainer.py to streamline model training process.

--- modelTraining/xgboost_trainer.py
+++ modelTraining/xgboost_trainer.py
@@ -42,9 +42,6 @@ def run(target_Col):
     print(f'Training dataset: {X}')
 
     X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.3,random_state=42)
-    
-    xgb_train = xgb.DMatrix(X_train, y_train)
-    xgb_test = xgb.DMatrix(X_test, y_test)
 
     model = xgb.XGBRegressor(
         objective='reg:squarederror',
