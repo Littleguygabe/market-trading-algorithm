271fe349648f582ef76e9488a590ed8d5cfaf15d
Gabriel Bridger <106691358+Littleguygabe@users.noreply.github.com>
2025-09-06 01:08:45 +0100

    Implement LSTM model training functionality in LSTM_trainer.py, including data preprocessing with StandardScaler and train-test split. Add model architecture with LSTM layers, compile settings, and callbacks for early stopping and learning rate reduction. Update run function to accommodate new training method and ensure proper handling of target horizons.

--- modelTraining/LSTM_trainer.py
+++ modelTraining/LSTM_trainer.py
@@ -3,6 +3,13 @@ import os
 import argparse
 import numpy as np
 
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from keras.callbacks import ReduceLROnPlateau
+import joblib
+from pathlib import Path
+
 
 def panelData(df):
     columns = df.columns.values
@@ -59,11 +66,66 @@ def dataRead(target_col):
     df = df.dropna(subset=['Target'])
     return df
 
-def run(target_col):
+def trainModel(X,y,horizons):
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+    Xscaler = StandardScaler()
+    X_train = Xscaler.fit_transform(X_train.reshape(-1,X_train.shape[-1])).reshape(X_train.shape)
+    X_test = Xscaler.fit_transform(X_test.reshape(-1,X_test.shape[-1])).reshape(X_test.shape)
+
+    yscaler = StandardScaler()
+    y_train = yscaler.fit_transform(y_train)
+    y_test_scaled = yscaler.transform(y_test)
+
+        #build the model
+    model = keras.models.Sequential([
+        keras.layers.Input(shape=(X_train.shape[1], X_train.shape[2])),
+        keras.layers.LSTM(64, return_sequences=True),
+        keras.layers.LSTM(128, return_sequences=False),
+    ]) 
+    model.add(keras.layers.Dense(128,activation='relu'))
+    model.add(keras.layers.Dropout(0.4))
+    
+    
+    model.add(keras.layers.Dense(len(forecast_horizons)))
+
+    initial_lr = 0.01
+    optimiser = keras.optimizers.Adam(learning_rate = initial_lr)
+
+    model.compile(optimizer = optimiser,
+                loss='mae',
+                metrics=['mse'],
+                )
+
+    callback = keras.callbacks.EarlyStopping(monitor='loss',patience=5)
+
+    reduce_lr_callback = ReduceLROnPlateau(
+    monitor='val_loss',
+    factor=0.5,
+    patience=5,
+    verbose=1,
+    min_lr=1e-6
+)
+
+    model.fit(X_train,
+                        y_train,
+                        epochs=50,
+                        batch_size = 128,
+                        validation_split = 0.1,
+                        callbacks=[callback,reduce_lr_callback],
+                        # verbose=1
+                        )
+
+
+    loss,mse = model.evaluate(X_test,y_test_scaled)
+
+    return model,Xscaler,yscaler
+
+def run(target_col,horizons):
+
     data = dataRead(target_col)
     X,y = panelData(data)
+    model,X_scaler,Y_scaler = trainModel(X,y,horizons)
 
-    
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Train an LSTM model to forecast stock prices.')
@@ -77,5 +139,5 @@ if __name__=='__main__':
     target_column = args.target
     print('>--------------------')
     print('Running LSTM Algorithm')
-    run(target_column)
+    run(target_column,forecast_horizons)
 
